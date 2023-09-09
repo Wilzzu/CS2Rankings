6e7c0542f1d247dee5d8e19154f9be9166780fe2
Wille Karvinen <wille.karvinen@student.lab.fi>
2023-09-09 03:22:53 +0300

    LB status, rank indicators, scrolling bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import "./App.css";
 import usePrefetchLeaderboard from "./hooks/usePrefetchLeaderboard";
 import MainContent from "./components/MainContent";
+import { motion, useScroll, useMotionValueEvent } from "framer-motion";
 
 // TODO: Show missing ranks as unknown/grayed out, since their name hasn't been approved yet
 
@@ -11,12 +12,24 @@ function App() {
 		prefetchLeaderboard();
 	}, [prefetchLeaderboard]);
 
+	const ref = useRef(null);
+	const { scrollYProgress } = useScroll({
+		target: ref,
+	});
+
+	useMotionValueEvent(scrollYProgress, "change", (latest) => {
+		if (latest >= 1) return;
+		ref.current.style.backgroundPositionX = latest * 4000 + "px";
+	});
+
 	return (
-		<div className="bg-cswhite min-h-[100dvh]">
+		<motion.div
+			ref={ref}
+			className="min-h-[100dvh] bg-[url('./assets/testbg.svg')] bg-fixed duration-500 ease-out ">
 			<div className="flex justify-center">
 				<MainContent />
 			</div>
-		</div>
+		</motion.div>
 	);
 }
 
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,19 +1,25 @@
 import useGetLeaderboard from "../hooks/useGetLeaderboard";
 import LeaderboardContent from "./LeaderboardContent";
 import LeaderboardColumnNames from "./LeaderboardColumnNames";
+import { useEffect } from "react";
 
 const MainContent = () => {
 	const { data, isLoading, isError, refetchLeaderboard } = useGetLeaderboard("world");
+
+	useEffect(() => {
+		if (isError) console.log("error");
+	}, [isError]);
 
 	// TODO: Button for toggling missing players on and off
 	// Region select https://www.npmjs.com/package/react-dropdown
 	// Top bar always stays on stop position sticky
 	// Refresh, player search, season select, region select
+	// Searching players shows all the players containing search phrase in a same kind of drop down like region, when clicked scroll to the player
 	return (
 		<div className="w-[768px]">
 			<button onClick={() => refetchLeaderboard()}>Refetch</button>
 			{/* Leaderboard */}
-			<div className="bg-cswhitebright p-2">
+			<div className="bg-cswhitebright p-2 min-h-[90dvh]">
 				<LeaderboardColumnNames />
 				<LeaderboardContent data={data} isLoading={isLoading} isError={isError} />
 			</div>
