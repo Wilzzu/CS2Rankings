10f63b287735ff93aa3136614d8c358ad3a82004
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2023-09-13 19:57:41 +0300

    Add info to unknown players

--- src/components/LeaderboardItem.jsx
+++ src/components/LeaderboardItem.jsx
@@ -1,6 +1,7 @@
 import { cn } from "../../lib/utils";
 import Rank from "./Rank";
 import RatingIcon from "./RatingIcon";
+import infoIcon from "../assets/infoIcon.svg";
 
 // const tierColor = [
 // 	"text-[#C7CFD3]",
@@ -23,7 +24,24 @@ const LeaderboardItem = (props) => {
 			{/* Rank */}
 			<Rank position={props.data.position} rank={props.data.rank} missing={props.data?.missing} />
 			{/* Name */}
-			<p className={`truncate`}>{props.data?.missing ? "Unknown player" : `${props.data.name}`}</p>
+			{props.data?.missing ? (
+				<div className="flex items-center">
+					<p>Unknown player</p>
+					{/* Info */}
+					<div className="relative group h-full w-8 flex items-center justify-center group">
+						<img src={infoIcon} alt="Info icon" className="w-[0.85rem] opacity-50" />
+						<div className="bg-csblue bg-opacity-90 text-cswhitebright text-xs p-2 px-5 left-8 absolute hidden group-hover:block min-w-[240px]">
+							<p>
+								{
+									"Unknown players haven't submitted a leaderboard name or Valve hasn't approved it yet"
+								}
+							</p>
+						</div>
+					</div>
+				</div>
+			) : (
+				<p className={`truncate`}>{props.data.name}</p>
+			)}
 			{/* Rating */}
 			<RatingIcon
 				score={props.data?.missing ? "?????" : props.data.formattedScore}
