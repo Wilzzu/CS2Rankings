a1c704a822843df59d02d46d7bb21120f6642ff1
Wilzzu <11214356+Wilzzu@users.noreply.github.com>
2023-09-13 22:19:39 +0300

    Add privacy policy, responsive leaderboard

--- src/pages/Privacy.jsx
+++ src/pages/Privacy.jsx
@@ -1,6 +1,50 @@
+import { Link } from "react-router-dom";
+
 const Privacy = () => {
-	// TODO: Finish privacy policy
-	return <p>Privacy Policy</p>;
+	return (
+		<div className="min-h-[100dvh] w-full bg-[url('./assets/background.svg')] bg-repeat-x bg-cover bg-fixed flex justify-center pt-20">
+			<div className="w-full max-w-[768px] h-full bg-cswhitebright p-4 flex flex-col gap-6 text-lg font-poppins text-darktext">
+				<h1 className="text-2xl">Privacy Policy for CS2Rankings</h1>
+				<p>
+					CS2Rankings <b>does not</b> collect or store <b>any</b> of your data, or track your usage
+					in any way. We also <b>do not</b> use any cookies.
+				</p>
+				<p>
+					<b>However</b>, we do use <b>Cloudflare</b>, a third party service, to protect our website
+					from abuse. Cloudflare may collect data or set temporary cookies on your device. You can
+					view{" "}
+					<a
+						href="https://www.cloudflare.com/privacypolicy/"
+						target="_blank"
+						rel="noreferrer"
+						className="text-blue-600 hover:underline hover:underline-offset-1">
+						{"Cloudflare's"} privacy policy here
+					</a>{" "}
+					and{" "}
+					<a
+						href="https://developers.cloudflare.com/fundamentals/reference/policies-compliances/cloudflare-cookies/"
+						target="_blank"
+						rel="noreferrer"
+						className="text-blue-600 hover:underline hover:underline-offset-1">
+						their cookie policy here
+					</a>
+					.{" "}
+				</p>
+				<p>
+					If you have additional questions or require more information about our Privacy Policy, do
+					not hesitate to contact us by sending an email to{" "}
+					<a
+						href="mailto:wilzzudev@gmail.com"
+						className="font-bold hover:underline hover:underline-offset-1">
+						wilzzudev@gmail.com
+					</a>
+				</p>
+				<Link to={"/"} className="text-blue-600 hover:underline hover:underline-offset-1">
+					Back to home page
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export default Privacy;
