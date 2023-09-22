7e67f7d1bcbc5235fe36b976d9c7a40833d5f39c
Wille Karvinen <wille.karvinen@student.lab.fi>
2023-09-22 03:47:07 +0300

    Add favicon and main logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,15 @@
+import mainlogo from "../assets/mainlogo.svg";
+
 const Header = () => {
 	return (
 		<div className="relative w-[768px] h-12 md:h-16 flex items-center pl-2">
 			<div className="bg-[#fed600] w-4 h-full -skew-x-12 mr-1 shadow-md" />
 			<div className="bg-[#4a68ff] w-4 h-full -skew-x-12 mr-1 shadow-md" />
+			<img
+				src={mainlogo}
+				alt="CS2Rankings logo"
+				className="absolute right-4 z-10 w-10 md:w-14 opacity-50"
+			/>
 			<div className="w-full h-full -mr-2 -skew-x-12 bg-cswhitebright shadow-md"></div>
 			<div className="w-4"></div>
 			<p className="text-darktext leading-7 text-[1.75rem] md:text-[2.8rem] mb-[-1px] font-header font-bold tracking-wider absolute left-[3.2rem] md:left-16 drop-shadow-header">
